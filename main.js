d227ab7c94113fb35e27651249857dbfc927dd5d
Edgar Avila <avilaedgar152@gmail.com>
2024-11-12 18:30:24 -0600

    Se corrige el sw

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ if('serviceWorker' in navigator){
 
     console.log("si tiene sw");
 
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
                            .then(res=>console.log('serviceWorker cargado correctamente', res))
                            .catch(err=> console.log('serviceWorker no se pudo registrar', err))
 
